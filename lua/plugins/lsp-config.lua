3e94062c16402bdf1d3b8f6b95b7a202f9ba135c
Alonso <alonsofmira@gmail.com>
2025-06-18 20:54:55 +0200

    fix: lsp-config updates

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -26,5 +26,8 @@ return {
     {
         "pmizio/typescript-tools.nvim",
         dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+        config = function()
+            require("typescript-tools").setup({})
+        end
     }
 }
